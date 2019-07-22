c7b4f41b92abddb79f290ac8103159d6c50a7c1a
Naila <nailakaliyeva@gmail.com>
2019-07-22 20:30:09 +0000

    just changed the bg color

--- src/js/component/Counter.js
+++ src/js/component/Counter.js
@@ -4,21 +4,21 @@ import ReactDOM from "react-dom";
 
 export default function Counter(props) {
 	return (
-		<div className="secondsParent row d-flex justify-content-center mx-5 bg-warning mt-5">
-			<i className="far fa-clock fa-4x m-3 mr-5" />
-			<div className="second col-2 bg-dark rounded m-1 my-3">
+		<div className="secondsParent row d-flex justify-content-center mx-5 mt-5 rounded">
+			<i className="far fa-clock fa-4x m-3 my-auto" />
+			<div className="second col-2 bg-dark rounded m-1 my-5">
 				<h1 className="text-warning">{props.last % 10}</h1>
 			</div>
-			<div className="second col-2 bg-dark rounded m-1 my-3">
+			<div className="second col-2 bg-dark rounded m-1 my-5">
 				<h1 className="text-warning">{props.secondLast % 10}</h1>
 			</div>
-			<div className="second col-2 bg-dark rounded m-1 my-3">
+			<div className="second col-2 bg-dark rounded m-1 my-5">
 				<h1 className="text-warning">{props.thirdLast % 10}</h1>
 			</div>
-			<div className="second col-2 bg-dark rounded m-1 my-3">
+			<div className="second col-2 bg-dark rounded m-1 my-5">
 				<h1 className="text-warning">{props.fourthLast % 10}</h1>
 			</div>
-			<div className="second col-2 bg-dark rounded m-1 my-3">
+			<div className="second col-2 bg-dark rounded m-1 my-5">
 				<h1 className="text-warning">{props.fifthLast % 10}</h1>
 			</div>
 		</div>
